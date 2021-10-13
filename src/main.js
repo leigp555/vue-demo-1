dc6649e96149d157133954e8ea11795a7cfe592a
leigp <907090585@qq.com>
2021-10-13 22:46:53 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
 import Vue from 'vue'
 import Demo from "./demo.vue"
+//
+// Vue.config.productionTip = false
+//
+//
+//
 
-Vue.config.productionTip = false
+//非完整版
 
-console.log(Demo.render.toString());
+// //引入方法runtime.js
+// new Vue({
+//     el: "#app",
+//     render(h) {
+//         return h(Demo)
+//     }
+// })
+
+//引入方法runtime.js
 new Vue({
-    el: "#app",
-    render(h) {
-        return h(Demo)
-    }
-})
+    render:h=>h(Demo)
+}).$mount("#app")
+console.log(Demo)
+
+
+//完整版使用
+
+// new Vue({
+//     el:"#app",
+//     template:`
+//       <div> {{n}} </div>`
+//     ,
+//     data:{n:0}
+// })
